6e13d3f21f4bba8b978f25011c996acd093bc71d
megha <meghapatel1.tops@gmail.com>
2026-03-19 16:12:57 +0530

    all

---
+++ reactjs/newapp/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Home from './Components/Home'
+import About from './Components/About'
+// import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <h1>Welcome to react app</h1>
+        <Home />
+        <About />
+    </>
+  )
+}
+
+export default App
---
+++ reactjs/newapp/src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h2>Home Page</h2>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur ab, maxime sunt vero doloremque voluptatum officiis voluptas omnis quae fugit iusto molestias minus itaque beatae soluta blanditiis sapiente corporis? Tempore.</p>
+    </div>
+  )
+}
+
+export default Home
